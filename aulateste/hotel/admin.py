ebd3faa31de0ed42a38c671e6dd9179132c8ec58
natinhaaa <natvcer2@gmail.com>
2024-05-14 08:37:52 -0300

    first commit

---
+++ aulateste/hotel/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import hotel, quarto
+
+# Register your models here.
+
+admin.site.register(hotel)
+admin.site.register(quarto)
---
+++ aulateste/hotel/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+import datetime
+
+# Create your models here.
+
+TIPOS_QUARTOS = (
+    ("SOLTEIRO", "Solteiro"),
+    ("CASAL", "Casal"),
+    ("COMFORT", "Comfort"),
+    ("LUXO", "Luxo")
+)
+
+class hotel(models.Model):
+    titulo = models.CharField(max_length=50)
+    descricao = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.titulo
+
+class quarto(models.Model):
+    tipo = models.CharField(max_length=15, choices=TIPOS_QUARTOS)
+    disponibilidade= models.IntegerField()
+    valor = models.FloatField(max_length=4)
+    descricao = models.TextField(max_length=200)
+    # data_reserva = models.DateTimeField(default=datetime.datetime.now)
+
+    def __str__(self):
+        return self.tipo
