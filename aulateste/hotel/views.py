2ab7d0b4d38aed8a7c754609ecf3838163482676
natinhaaa <natvcer2@gmail.com>
2024-05-14 22:16:03 -0300

    update alterando a tela homepage e tela para reserva, quartos

--- aulateste/hotel/views.py
+++ aulateste/hotel/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from .models import hotel
+from .models import quarto
+
 
 # Create your views here.
 
@@ -8,4 +10,10 @@ def homepage(request):
     dados_hotel = hotel.objects.all
     context['dados_hotel'] = dados_hotel
     return render(request, 'homepage.html', context)
+
+def reserve(request):
+    context = {}
+    dados_quarto = quarto.objects.all()
+    context['dados_quarto'] = dados_quarto
+    return render(request, 'reserve.html', context) #retorna a pagina quartos.html
 
