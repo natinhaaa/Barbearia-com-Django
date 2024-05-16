62783cc4bfee70c325b8d0e26d45ae1d5816ed91
natinhaaa <natvcer2@gmail.com>
2024-05-16 16:20:27 -0300

    update adicionando nome aos quartos e terminando a tela de cada quarto

--- aulateste/hotel/models.py
+++ aulateste/hotel/models.py
@@ -9,17 +9,35 @@ TIPOS_QUARTOS = (
     ("LUXO", "Luxo")
 )
 
+NOME_QUARTOS = (
+    ("SOLTEIRO CONFORTÁVEL", "Solteiro Confortável"),
+    ("SOLTEIRO COM VISTA", "Solteiro com Vista"),
+    ("SOLTEIRO ECONÔMICO", "Solteiro Econômico"),
+    ("CASAL LUXO", "Casal Luxo"),
+    ("SUÍTE ROMÂNTICA", "Suíte Romântica"),
+    ("CASAL STANDARD", "Casal Standard"),
+    ("SUPERIOR COM VISTA", "Superior com Vista"),
+    ("COMFORT CLÁSSICO", "Comfort Clássico"),
+    ("COMFORT DELUXE", "Comfort Deluxe"),
+    ("COMFORT EXECUTIVO", "Comfort Executivo"),
+    ("COMFORT FAMILIAR", "Comfort Familiar"),
+    ("SUÍTE LUXO EXECUTIVO", "Suíte Luxo Executivo"),
+    ("SUÍTE PRESIDENCIAL", "Suíte Presidencial"),
+    ("LUXO PREMIUM", "Luxo Premium"),
+    ("SUÍTE REAL", "Suíte Real"),
+)
+
 class hotel(models.Model):
     titulo = models.CharField(max_length=50)
     descricao = models.TextField(max_length=500)
-    logo = models.ImageField(upload_to="logo/")
+    foto_hotel = models.ImageField(upload_to="Foto_Hotel/")
 
     def __str__(self):
         return self.titulo
 
 class quarto(models.Model):
     tipo = models.CharField(max_length=15, choices=TIPOS_QUARTOS)
-    nome = models.CharField(max_length=20)
+    nome = models.CharField(max_length=20, choices=NOME_QUARTOS)
     disponibilidade= models.IntegerField()
     valor = models.FloatField(max_length=4)
     descricao = models.TextField(max_length=230)
